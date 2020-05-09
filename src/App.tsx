cc99423a8e89ee8b18f807f70f51f0e51fa19813
Alex <grok88@tut.by>
2020-05-09 22:53:07 +0300

    lesson 2 is done

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
 import React from 'react';
 import './App.css';
+import {TaskType, TodoList} from "./TodoList";
 
 function App() {
+    const tasks1:Array<TaskType> = [
+        {id: 1, title: "CSS", isDone: true},
+        {id: 2, title: "JS", isDone: true},
+        {id: 3, title: "REACT", isDone: false}
+    ];
+    const tasks2:Array<TaskType> = [
+        {id: 1, title: "Milk", isDone: true},
+        {id: 2, title: "Bread", isDone: true},
+        {id: 3, title: "The Earth", isDone: false}
+    ];
+
     return (
         <div className="App">
-            <div>
-                <h3>What to learn</h3>
-                <div>
-                    <input/>
-                    <button>+</button>
-                </div>
-                <ul>
-                    <li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
-                    <li><input type="checkbox" checked={true}/> <span>JS</span></li>
-                    <li><input type="checkbox" checked={false}/> <span>React</span></li>
-                </ul>
-                <div>
-                    <button>All</button>
-                    <button>Active</button>
-                    <button>Completed</button>
-                </div>
-            </div>
+            <TodoList title={"What to learn"} tasks={tasks1}/>
+            <TodoList title={"What to buy"} tasks={tasks2} />
+            {/*<TodoList title={"What to sell"}/>*/}
         </div>
     );
 }
