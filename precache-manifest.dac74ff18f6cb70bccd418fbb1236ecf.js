73971df1af880687e6a5644b672c9f6255d52121
Alex <grok88@tut.by>
2020-10-22 21:34:38 +0300

    Updates

---
+++ precache-manifest.dac74ff18f6cb70bccd418fbb1236ecf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20c242d53374fb95d8eb6ffc5fb7c512",
+    "url": "/TodoList_new/index.html"
+  },
+  {
+    "revision": "885339e5839c7625c27e",
+    "url": "/TodoList_new/static/css/main.2062bf85.chunk.css"
+  },
+  {
+    "revision": "e6ea2905b91f0ed65ff9",
+    "url": "/TodoList_new/static/js/2.7168c6c2.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/TodoList_new/static/js/2.7168c6c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "885339e5839c7625c27e",
+    "url": "/TodoList_new/static/js/main.672ac4ec.chunk.js"
+  },
+  {
+    "revision": "1f80582c83266974d575",
+    "url": "/TodoList_new/static/js/runtime-main.5b747b87.js"
+  }
+]);
